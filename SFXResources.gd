952942e6e43039a1e96883d5cf147210f49499fc
Olin Gallet <olindgallet@gmail.com>
2026-03-20 18:12:11 -0500

    Delete SFXResources.gd

--- SFXResources.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name SFXResources
-enum SFX {
-	TITLE_THUD
-}
-static func path(route: int) -> String:
-	match route:
-		SFX.TITLE_THUD: return "res://ui/title/thud.wav";
-		_: return "";
